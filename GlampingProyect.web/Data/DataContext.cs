d03f90d4e5b51165da4c5bc67acc474d38afb855
JuanCGonzalez10 <juancamilogonzalezh11.1@gmail.com>
2025-04-06 18:26:45 -0500

    Layout p2 sections con entityframeworkcore

---
+++ GlampingProyect.web/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿
+using GlampingProyect.web.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+namespace GlampingProyect.web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options) : base(options)
+           {
+           }
+
+        public DbSet<Section> Sections { get; set; }
+    }
+}
---
+++ GlampingProyect.web/Data/Entities/Section.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GlampingProyect.web.Data.Entities
+{
+    public class Section
+    {
+        [key]
+        public int Id { get; set; }
+
+        [Display(Name = "seccion")]
+        [Required(ErrorMessage = "el campo '{0}' es requerido")]
+        public string Name { get; set; } = null!;
+
+        [Display(Name = "Descripcion")]
+
+        public string? Descripcion { get; set; }
+
+        [Display(Name = "Esta oculta?")]
+        public bool IsHidden { get; set; }
+
+
+    }
+}
